a47583b5bd6a79211fc851b68388bbd69f31326e
Hamdi <melikaebrahim0@gmail.com>
2022-12-16 15:21:31 -0800

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,12 @@
 
 /**
  * mul -> multiplying 2 numbers
- * @a: integer param 1
+ * @a: integer param1
  * @b: integer param 2
- * Return: success and intiger
+ * Return: success and integer
  */
 
 int mul(int a, int b)
 {
-	return (a * b)
+	return (a * b);
 }	
