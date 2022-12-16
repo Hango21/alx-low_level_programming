39ce9b22084cc0f783934a5cb5982112d50db5cd
Hamdi <melikaebrahim0@gmail.com>
2022-12-16 12:14:33 -0800

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if intigeris positive or negative
+ * main - Test function for positive or negative
  * Return: 0
  */
 
@@ -9,8 +9,9 @@ int main(void)
 {
 	int i;
 
-	i = o;
+	i = 0;
 	positive_or_negative(i);
-
-	return (0);
+	
+return (0);
 }
+
