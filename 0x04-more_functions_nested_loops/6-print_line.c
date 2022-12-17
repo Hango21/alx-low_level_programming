d562bc251c99a6546aeda4c894533922ddfab98b
Hamdi <melikaebrahim0@gmail.com>
2022-12-16 17:24:11 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,18 +3,17 @@
 /**
  * print_line - Draws a straight line using a character __.
  * @n: the number of _ character to be printed out.
+ *
+ * Return: void
  */
 
 void print_line(int n)
 {
-	int line;
+	int i;
 
-	if (n > 0)
-	{
-	for (line = 0; line <= n; line++)
+	for (i = 0; i <= n; i++)
 	{
 	_putchar('_');
-	}
 	}
 	_putchar('\n');
 }
